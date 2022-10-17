a1116ed05c21b8a90254a879a368c475a9d9f3f8
Sebastian Pape <s@pape.io>
2022-10-17 19:55:38 +0200

    v9.8.0: multiprovider + evm entry point fixes

--- dist/esm/index.evm.js
+++ dist/esm/index.evm.js
@@ -14818,32 +14818,39 @@ const setProviderEndpoints$4 = (blockchain, setProvider)=> {
   
   return async (endpoints)=> {
     
-    let responseTimes = await Promise.all(endpoints.map((endpoint)=>{
-      return new Promise(async (resolve)=>{
-        let timeout = 900;
-        let before = new Date().getTime();
-        setTimeout(()=>resolve(timeout), timeout);
-        await fetch(endpoint, {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ method: 'net_version', id: 1, jsonrpc: '2.0' })
-        });
-        if(response != 'ok') { return resolve(999) }
-        let after = new Date().getTime();
-        resolve(after-before);
-      })
-    }));
+    let endpoint;
+    let window = getWindow();
 
-    const fastestResponse = Math.min(...responseTimes);
-    const fastestIndex = responseTimes.indexOf(fastestResponse);
+    if(window.fetch != undefined) {
+      
+      let responseTimes = await Promise.all(endpoints.map((endpoint)=>{
+        return new Promise(async (resolve)=>{
+          let timeout = 900;
+          let before = new Date().getTime();
+          setTimeout(()=>resolve(timeout), timeout);
+          await fetch(endpoint, {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ method: 'net_version', id: 1, jsonrpc: '2.0' })
+          });
+          if(response != 'ok') { return resolve(999) }
+          let after = new Date().getTime();
+          resolve(after-before);
+        })
+      }));
+
+      const fastestResponse = Math.min(...responseTimes);
+      const fastestIndex = responseTimes.indexOf(fastestResponse);
+      endpoint = endpoints[fastestIndex];
+    } else {
+      endpoint = endpoints[0];
+    }
     
     setProvider(
-      new StaticJsonRpcBatchProvider(
-        endpoints[fastestIndex], blockchain
-      )
+      new StaticJsonRpcBatchProvider(endpoint, blockchain)
     );
   }
 };
