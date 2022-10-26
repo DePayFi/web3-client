f2b66b4597c1944f536ec603aaf42ae981f08b36
Sebastian Pape <s@pape.io>
2022-10-26 10:57:39 +0200

    v10.1.15: remove cloudflare for now

--- dist/esm/index.js
+++ dist/esm/index.js
@@ -15198,7 +15198,7 @@ let getWindow = () => {
 };
 
 const ENDPOINTS$1 = {
-  ethereum: ['https://cloudflare-eth.com', 'https://eth-mainnet.public.blastapi.io', 'https://ethereum.publicnode.com'],
+  ethereum: ['https://rpc.ankr.com/eth', 'https://eth-mainnet.public.blastapi.io', 'https://ethereum.publicnode.com'],
   bsc: ['https://bsc-dataseed.binance.org', 'https://bsc-dataseed1.ninicoin.io', 'https://bsc-dataseed3.defibit.io'],
   polygon: ['https://polygon-rpc.com', 'https://rpc-mainnet.matic.quiknode.pro', 'https://matic-mainnet.chainstacklabs.com'],
   velas: ['https://mainnet.velas.com/rpc', 'https://evmexplorer.velas.com/rpc', 'https://explorer.velas.com/rpc'],
