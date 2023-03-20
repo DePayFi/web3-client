48546dc8a4b4803dba88259a49160687c471ffdf
Sebastian Pape <s@pape.io>
2023-03-20 09:11:50 +0100

    v10.6.1: fix solana package

---
+++ rollup.module.solana.config.js
@@ -0,0 +1,47 @@
+import commonjs from '@rollup/plugin-commonjs'
+import globals from './rollup.globals.js'
+import jscc from 'rollup-plugin-jscc'
+import json from '@rollup/plugin-json'
+import pkg from './package.solana.json'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import sucrase from '@rollup/plugin-sucrase'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      format: 'es',
+      globals: globals,
+      file: 'dist/esm/index.solana.js'
+    },
+    {
+      format: 'umd',
+      name: pkg.moduleName,
+      globals: globals,
+      file: 'dist/umd/index.solana.js'
+    }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    jscc({ values: { _SOLANA: 1 }}),
+    json(),
+    sucrase({
+      exclude: ['node_modules/**'],
+      transforms: ['jsx']
+    }),
+    resolve(),
+    nodeResolve(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify( 'production' ),
+      preventAssignment: true
+    })
+  ]
+}
